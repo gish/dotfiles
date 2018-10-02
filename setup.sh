b9cbc4b1be829807a03b573cab0373f6c6b7eaad
Erik Hedberg <erik@hedberg.at>
2018-10-02 20:11:31 +0200

    setup: Add script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+brew update
+brew install\
+  tmux\
+  git\
+  neovim\
+  diff-so-fancy
+
+# tmux
+## TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s "`pwd`/tmux.conf" ~/.tmux
+
+# git
+## Setup git diff tool
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+
+# vim
+## Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+nvim +PluginInstall +qall
+ln -s "`pwd`/nvim" ~/.vimrc
+
+# zsh
+ln -s "`pwd`/zshrc" ~/.zshrc
